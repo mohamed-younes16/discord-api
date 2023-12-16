d11abb62744a77fbb1432cb59a3d2839ea70b58e
mohamed-younes16 <bettiouim77@gmail.com>
2023-12-16 08:01:32 +0100

    better

--- src/config/corsOptions.js
+++ src/config/corsOptions.js
@@ -1,8 +1,9 @@
 
-const allowedlist =  ["https://chat.openai.com","http://localhost:3500","https://www.youtube.com","http://localhost:3000"]
+const allowedlist =  ["https://chat.openai.com","http://localhost:3500"
+,"https://www.youtube.com","http://localhost:3000","https://discord-clone-pro.up.railway.app"]
 
 const allowedHeaders = (origin,call)=>{
-
+console.log(origin)
 allowedlist.includes(origin) || !origin ?call(null , true):
 call(new Error("do i know you ?"))
 }
